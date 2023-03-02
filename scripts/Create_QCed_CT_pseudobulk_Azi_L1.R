f158e27fbd0ae7d8b39f303d0c35dcc92a55b0cb
Marc Jan Bonder <bonder.m.j@gmail.com>
2023-03-02 16:30:30 +0100

    Add files via upload

--- scripts/Create_QCed_CT_pseudobulk_Azi_L1.R
+++ scripts/Create_QCed_CT_pseudobulk_Azi_L1.R
@@ -8,8 +8,12 @@ option_list = list(
               help="WG1 seurat object out (singlets and filtered)."),
   make_option(c("--metadata"), action="store", default=NA, type='character',
               help="MAD filter and donor annotation information."),
-    make_option(c("--wg1_psam"), action="store", default=NA, type='character',
+  make_option(c("--wg1_psam"), action="store", default=NA, type='character',
               help="psam file created in WG1."),
+  make_option(c("--cell_annotation"), action="store", default=NA, type='character',
+              help="cell specific annotation."),
+  make_option(c("--wg1_imputed_genotypes"), action="store", default=NA, type='character',
+              help="sample genotype stats file."),
   make_option(c("--cell_level"), action="store", default="l1", type='character',
               help="Azimuth l1 or l2."),
   make_option(c("--aggregate_fun"), action="store", default='mean', type='character',
@@ -31,6 +35,8 @@ shhh(library(pbapply))
 print(paste0('input1: ', opt$wg1_data))
 print(paste0('input2: ', opt$wg1_psam))
 print(paste0('input3: ', opt$metadata))
+print(paste0('input4: ', opt$wg1_imputed_genotypes))
+print(paste0('input5: ', opt$cell_annotation))
 print(paste0('Azimuth level: ', opt$cell_level))
 print(paste0('Aggegate function: ', opt$aggregate_fun))
 cat('\n\n')
@@ -41,10 +47,20 @@ pbmc_fn <- opt$wg1_data
 print(paste0('Reading seurat object: ',pbmc_fn))
 system.time(pbmc <- readRDS(pbmc_fn))
 
-# Seurat object
+# psam file
 pbmc_psam_fn <- opt$wg1_psam
 print(paste0('Reading psam info: ',pbmc_psam_fn))
 system.time(psamMetaD <- read.delim(pbmc_psam_fn,as.is=T,check.names=F))
+
+# imputation sample statistics file
+pbmc_imputation_stats_fn <- opt$wg1_imputed_genotypes
+print(paste0('Reading sample genotype statistics: ',pbmc_imputation_stats_fn))
+system.time(genotypeSampleStats <- read.delim(pbmc_imputation_stats_fn,as.is=T,check.names=F))
+
+# cell annotation file
+pbmc_cell_annotation_fn <- opt$cell_annotation
+print(paste0('Reading cell annotation: ',pbmc_cell_annotation_fn))
+system.time(cellAnnotation <- read.delim(pbmc_cell_annotation_fn,as.is=T,check.names=F))
 
 # metadata object
 pbmc_metadata_fn <- opt$metadata
@@ -57,26 +73,40 @@ rm(pbmc_fn,pbmc_metadata_fn,pbmc_psam_fn)
 pbmcMetaD["celltype.l1"] = pbmcMetaD$predicted.celltype.l1
 pbmcMetaD$celltype.l1[which(pbmcMetaD$celltype.l1!=pbmcMetaD$scpred.l1.prediction)] = NA
 
-#Filtering to cells to keep.
+#Filtering to cells to keep, based on cell type and tag.
 pbmcMetaD  = pbmcMetaD[which(pbmcMetaD$tag=="NotOutlier"),]
 pbmcMetaD  = pbmcMetaD[which(pbmcMetaD$celltype.l1 != "Doublet"),]
 #pbmcMetaD  = pbmcMetaD[which(pbmcMetaD$celltype.l1 %in% c("CD4_T","Mono","CD8_T","NK","B","DC","other_T")),]
 
+##Filter psamMetaD to only europeans
+psamMetaD = psamMetaD[which(psamMetaD$IID %in% genotypeSampleStats$ID),]
+rm(genotypeSampleStats)
+
+#Drop cells treated with a stimulation.
+cellAnnotation = cellAnnotation[which(cellAnnotation$cell_treatment=="UT"),]
+
 #Make sure the entries match.
+print(paste("Dropped because of missing genotype info: ",length(which(!(pbmcMetaD$Assignment %in% psamMetaD$IID)))))
+pbmcMetaD  = pbmcMetaD[which(pbmcMetaD$Assignment %in% psamMetaD$IID),]
+print(paste("Dropped because of missing cell annotation info: ",length(which(!(pbmcMetaD$Barcode %in% cellAnnotation$Barcode)))))
+pbmcMetaD  = pbmcMetaD[which(pbmcMetaD$Barcode %in% cellAnnotation$Barcode),] 
+print(paste("Dropped because of missing expression data: ",length(which(!(pbmcMetaD$Barcode %in% colnames(pbmc))))))
 pbmcMetaD  = pbmcMetaD[which(pbmcMetaD$Barcode %in% colnames(pbmc)),]
 pbmc = pbmc[,which(colnames(pbmc) %in% pbmcMetaD$Barcode)]
+cellAnnotation = cellAnnotation[which(cellAnnotation$Barcode %in% pbmcMetaD$Barcode),]
 
 pbmc = pbmc[,order(colnames(pbmc))]
 pbmcMetaD = pbmcMetaD[match(colnames(pbmc),pbmcMetaD$Barcode),]
-pbmcMetaD = cbind(pbmcMetaD,paste0(pbmcMetaD$celltype.l1,";;",pbmcMetaD$Pool,";;",pbmcMetaD$Assignment))
-colnames(pbmcMetaD)[ncol(pbmcMetaD)] ="CT_Pool_Donor"
-
-pbmcMetaD = cbind(pbmcMetaD,paste0(pbmcMetaD$Assignment,";;",pbmcMetaD$Pool))
-colnames(pbmcMetaD)[ncol(pbmcMetaD)] ="Donor_Pool"
+cellAnnotation = cellAnnotation[match(colnames(pbmc),cellAnnotation$Barcode),]
 
 ##Extend meta_data with info from psam.
 pbmcMetaD = cbind(pbmcMetaD,psamMetaD[match(pbmcMetaD$Assignment,psamMetaD$IID),])
-relCol = c("Donor_Pool",colnames(psamMetaD))
+pbmcMetaD = cbind(pbmcMetaD,cellAnnotation[match(pbmcMetaD$Barcode,cellAnnotation$Barcode),])
+
+pbmcMetaD$Pool = paste0(pbmcMetaD$sequencing_run,"_",pbmcMetaD$sequencing_lane)
+pbmcMetaD = cbind(pbmcMetaD,paste0(pbmcMetaD$Assignment,";;",pbmcMetaD$Pool))
+colnames(pbmcMetaD)[ncol(pbmcMetaD)] ="Donor_Pool"
+relCol = unique(c("Donor_Pool",colnames(psamMetaD),colnames(cellAnnotation)[2:ncol(cellAnnotation)]))
 aggregate_fun = opt$aggregate_fun
 
 if(aggregate_fun!='mean'){
@@ -198,81 +228,3 @@ if(all(colnames(pbmc)==pbmcMetaD$Barcode)){
   colnames(smf)=c("genotype_id","phenotype_id")
   write.table(unique(tmpMd[,c(5,32)]),paste0(gsub("L1","",opt$out_dir),"/smf.txt"),quote=F,sep="\t",row.names=F)
 }
-
-
-
-##########Original
-#if(all(colnames(pbmc)==pbmcMetaD$Barcode)){
-#  pbmc@meta.data = pbmcMetaD
-#  
-#  ## grab the countmatrix
-#  countMatrix <- GetAssayData(pbmc, slot = "counts")
-#  countMatrix = countMatrix[which(rowSums(countMatrix)!=0),]
-#  IDs = pbmc@meta.data$CT_Donor
-#  unique_ID_list = unique(pbmc@meta.data$CT_Donor)
-#  
-#  # Info
-#  n_cells <- ncol(countMatrix)
-#  n_genes <- nrow(countMatrix)
-#  print(paste0('Calculating pseudo-bulk expression matrix using: ', aggregate_fun))
-#  print(paste0('   ### n_cells: ', n_cells))
-#  print(paste0('   ### n_genes (sc-data): ', n_genes))
-#  cat('\n')#
-#
-#  print('Normalizing the sc-data..')
-#  cat('\n')
-#  
-#  # PF
-#  sampleSumInfo = colSums(countMatrix)
-#  meanSampleSum = mean(sampleSumInfo)
-#  sampleScale = sampleSumInfo/meanSampleSum
-#  
-#  # Scale to match meanRowSums
-#  system.time(countMatrix <- sweep(countMatrix, 2, sampleScale, FUN="/")) #divide each column by sampleScale
-#  
-#  # Log
-#  countMatrix = log(countMatrix+1)
-#  
-#  # PF
-#  sampleSumInfo = colSums(countMatrix)
-#  meanSampleSum = mean(sampleSumInfo)
-#  sampleScale = sampleSumInfo/meanSampleSum
-#  
-#  # Scale to match meanRowSums
-#  system.time(countMatrix <- sweep(countMatrix, 2, sampleScale, FUN="/")) #divide each column by sampleScale
-#  
-#  print('Aggregating count matrix using lapply + textTinyR::sparse_Means() ...')
-#  countMatrix <- as(countMatrix, "dgCMatrix")
-#  
-#  system.time(aggregate_countMatrix <- as.data.frame(pblapply(unique_ID_list, FUN = function(x){sparse_Means(countMatrix[,IDs == x, drop = FALSE], rowMeans = TRUE)})))
-#  cellcount <- pblapply(unique_ID_list, FUN = function(x){ncol(countMatrix[,IDs == x, drop = FALSE])})
-#  colnames(aggregate_countMatrix) <- names(cellcount) <- unique_ID_list
-#  rownames(aggregate_countMatrix) <- rownames(countMatrix)
-#  
-#  ##Filter to 10 cells minimum.
-#  cellCount = unlist(cellcount)
-#  aggregate_countMatrix = aggregate_countMatrix[,which(colnames(aggregate_countMatrix) %in% names(which(cellCount>9)))]
-#  ##Split per L1 cell type
-#  ctList = unique(unlist(lapply(strsplit(colnames(aggregate_countMatrix),split=";;"),'[[',1)))
-#  for(ct in ctList){
-#    ##select relevant columns
-#    selMatrix = aggregate_countMatrix[,grep(ct,colnames(aggregate_countMatrix))]
-#    colnames(selMatrix) = unlist(lapply(strsplit(colnames(selMatrix),split=";;"),'[[',2))
-#    if(ncol(selMatrix)>15){
-#      print(paste0("Writing: ",ct))
-#      ##Drop rows that are not varying, which will include genes that are only zero.
-#      rowVarInfo = rowVars(as.matrix(selMatrix))
-#      selMatrix = selMatrix[which(rowVarInfo!=0),]
-#      ## Do inverse normal transform per gene.
-#      for(rN in 1:nrow(selMatrix)){
-#         selMatrix[rN,] = qnorm((rank(selMatrix[rN,],na.last="keep")-0.5)/sum(!is.na(selMatrix[rN,])))
-#      }
-#      ##Do PCA, and select first 10 components.
-#      pcOut = prcomp(t(selMatrix))
-#      covOut = pcOut$x[,1:10]
-#      ##Write out PCs and input matrix for QTL.
-#      write.table(selMatrix,paste0(opt$out_dir,"/",ct,".inputExpression.txt"),quote=F,sep="\t",col.names=NA)
-#      write.table(covOut,paste0(opt$out_dir,"/",ct,".covariates.txt"),quote=F,sep="\t",col.names=NA)
-#    }
-#  }
-#}
