dd17908efcd9a8ce4e710c5992ec52f5719e85c4
Marc Jan Bonder <bonder.m.j@gmail.com>
2023-04-21 08:37:36 +0200

    Update define_windows.py

--- scripts/define_windows.py
+++ scripts/define_windows.py
@@ -65,6 +65,11 @@ for i in range(gene_info.shape[0]):
       block = win_start + window_size
       end = int(gene_info['end'].iloc[i])
 
+##Adding the last block
+win_end = end + gene_window
+windows[str(n)] = {}
+windows[str(n)]['range'] = [win_start, win_end]
+windows[str(n)]['SNPs'] = []
 
 ##### WRITE EACH WINDOW TO FILE #####
 
